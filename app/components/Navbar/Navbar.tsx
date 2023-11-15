278ced4e45cc21e8bb9ab4c291640fa7144359c9
Bobby Naluz <bnaluz9@gmail.com>
2023-11-14 21:35:40 -0500

    nav style update

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -11,7 +11,7 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
-    <div className="fixed w-full bg-[#B6D0E2] z-10 shadow-sm mb-10">
+    <div className="fixed w-full bg-[#214258] z-10 shadow-sm mb-10">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md-gap-0">
--- app/components/inventory/PaginationControls.tsx
+++ app/components/inventory/PaginationControls.tsx
@@ -23,7 +23,7 @@ const PaginationControls: React.FC<PaginationControlsProps> = ({
     <>
       <div className="flex items-center justify-between pt-4">
         <button
-          className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-[#214258] dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white"
           disabled={!hasPrevPage}
           onClick={() =>
             router.push(`/?page=${Number(page) - 1}&per_page=${per_page}`)
@@ -37,7 +37,7 @@ const PaginationControls: React.FC<PaginationControlsProps> = ({
         </div>
 
         <button
-          className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:dark:bg-[#214258] dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white"
           disabled={!hasNextPage}
           onClick={() =>
             router.push(`/?page=${Number(page) + 1}&per_page=${per_page}`)
