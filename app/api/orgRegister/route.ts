5639265912c186b236f4aef2b73d4f31f3fb7989
Bobby Naluz <bnaluz9@gmail.com>
2024-01-09 21:27:45 -0500

    adjust org signup

--- app/api/orgRegister/route.ts
+++ app/api/orgRegister/route.ts
@@ -4,12 +4,13 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const { email, name, password } = body;
+  const { email, name, password, organization } = body;
 
   const hashedPassword = await bcrypt.hash(password, 12);
 
   const user = await prisma.user.create({
     data: {
+      organization,
       email,
       name,
       hashedPassword,
